46fe01f665ad3dc252914d1eb864a4a669ed02f8
ScrumRoman <roman.tsiupiak@scrumlaunch.com>
2023-09-20 18:28:42 +0300

    init commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,56 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+    'plugin:import/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: [".eslintrc.cjs", "vite.config.ts", "tailwind.config.js"],
+  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
+    react: {
+      version: '18.x',
+    },
+  },
+  rules: {
+    'linebreak-style': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        extendDefaults: true,
+        types: {
+          '{}': false,
+        },
+      },
+    ],
+    'react-hooks/exhaustive-deps': 'off',
+    'object-shorthand': 'error',
+    'no-console': 'warn',
+    'import/default': 'off'
+  },
+};
---
+++ src/App/App.tsx
@@ -0,0 +1,7 @@
+import './styles.css';
+
+const App = () => {
+  return <div>Hello</div>;
+};
+
+export default App;
